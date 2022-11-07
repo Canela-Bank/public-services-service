c54093583114eb8c85aa78dae1abc1d1928acb56
estefania_bermudeza <estefanibearroyo@gmail.com>
2022-11-07 18:28:14 -0500

    Fix connection with GraphQL

--- src/main/java/com/canela/service/publicservices/controllers/ServicePaymentSavingsController.java
+++ src/main/java/com/canela/service/publicservices/controllers/ServicePaymentSavingsController.java
@@ -27,7 +27,7 @@ public class ServicePaymentSavingsController {
         //GraphQL connection
         URL getAccountUrl = null;
         try {
-            getAccountUrl = new URL("http://10.1.0.0:3001/graphql?query=%7B%0A%20%20getAccountById%20(id%3A%" + request.getSavingsId() + ")%7B%0A%20%20%20%20id%0A%20%20%20%20balance%0A%20%20%20%20user_id%0A%20%20%7D%0A%7D%0A");
+            getAccountUrl = new URL("http://10.1.0.0:3001/graphql?query=%7B%0A%20%20getAccountById%20(id%3A%22"+ request.getSavingsId() +"%22)%7B%0A%20%20%20%20id%0A%20%20%20%20balance%0A%20%20%20%20user_id%0A%20%20%7D%0A%7D%0A");
             HttpURLConnection connAccount = (HttpURLConnection) getAccountUrl.openConnection();
             connAccount.setRequestMethod("GET");
 
@@ -55,9 +55,9 @@ public class ServicePaymentSavingsController {
                 } else {
                     long newBalance = (Long.parseLong((String) jsonAccount.get("balance")) - salesService);
                     //Update balance of the account
-                    URL updateAccount = new URL("http://10.1.0.0:3001/graphql?query=mutation%7B%0A%20%20%20createAccount(id%3A%"+ request.getSavingsId() +"%22%2C%20balance%3A%20"+ newBalance +"%2C%20user_id%3A%20%22"+ request.getUserId() +"%22)%7B%0A%20%20%20%20id%2C%0A%20%20%20%20balance%2C%0A%20%20%20%20user_id%0A%20%20%7D%0A%7D%0A");
+                    URL updateAccount = new URL("http://10.1.0.0:3001/graphql?query=mutation%7B%0A%20%20createAccount%20(id%3A%22" + request.getSavingsId() + "%22%2C%20balance%3A%20" + newBalance + "%2C%20user_id%3A%20%22" + request.getUserId() + "%22)%7B%0A%20%20%20%20id%0A%20%20%20%20balance%0A%20%20%20%20user_id%0A%20%20%7D%0A%7D%0A");
                     HttpURLConnection connUpdate = (HttpURLConnection) updateAccount.openConnection();
-                    connUpdate.setRequestMethod("GET");
+                    connUpdate.setRequestMethod("POST");
 
                     if(connUpdate.getResponseCode() == HttpURLConnection.HTTP_OK){
                         return ResponseEntity.status(HttpStatus.ACCEPTED).body("Pago realizado");
