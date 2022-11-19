8b09d8fd1cfe6955bd3ce7dbf274006607e8127b
estefania_bermudeza <estefanibearroyo@gmail.com>
2022-11-18 20:48:37 -0500

    Fix cors

--- src/main/java/com/canela/service/publicservices/controllers/ServicePaymentSavingsController.java
+++ src/main/java/com/canela/service/publicservices/controllers/ServicePaymentSavingsController.java
@@ -19,11 +19,10 @@ import java.net.ProtocolException;
 import java.net.URL;
 
 @RestController
-@RequestMapping(value = "/api/service-payment")
+@RequestMapping(value = "/api/service-payments")
 public class ServicePaymentSavingsController {
 
     @PutMapping(value = "/savings")
-    @CrossOrigin("*")
     public ResponseEntity<String> servicePaymentSavings (@RequestBody ServiceRequest request){
 
     	String BillId = request.accountId;
@@ -31,7 +30,7 @@ public class ServicePaymentSavingsController {
    	
     	//Provider connection
     	try {
-    		URL getPriceUrl = new URL("http://localhost:8080/"+ request.serviceType +"/pay");
+    		URL getPriceUrl = new URL("http://localhost:9016/"+ request.serviceType +"/pay");
         	HttpURLConnection httpCon = (HttpURLConnection) getPriceUrl.openConnection();
         	httpCon.setDoOutput(true);
         	httpCon.setRequestMethod("POST");
@@ -101,9 +100,9 @@ public class ServicePaymentSavingsController {
                 if(salesService > Integer.parseInt(jsonAccount.get("balance").toString())){
                     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Pago rechazado");
                 } else {
-                    long newBalance = (Long.parseLong((String) jsonAccount.get("balance")) - salesService);
+                    long newBalance = (Long.parseLong(jsonAccount.get("balance").toString()) - salesService);
                     //Update balance of the account
-                    URL updateAccount = new URL("http://10.1.0.0:3001/graphql?query=mutation%7B%0A%20%20createAccount%20(id%3A%22" + request.getSavingsId() + "%22%2C%20balance%3A%20" + newBalance + "%2C%20user_id%3A%20%22" + request.getUserId() + "%22)%7B%0A%20%20%20%20id%0A%20%20%20%20balance%0A%20%20%20%20user_id%0A%20%20%7D%0A%7D%0A");
+                    URL updateAccount = new URL("http://10.1.0.0:3001/graphql?query=mutation%7B%0A%20%20createAccount%20(id%3A%22" + request.getSavingsId() + "%22%2C%20balance%3A%20" + newBalance + "%2C%20user_id%3A%20%22" + jsonAccount.get("user_id") + "%22)%7B%0A%20%20%20%20id%0A%20%20%20%20balance%0A%20%20%20%20user_id%0A%20%20%7D%0A%7D%0A");
                     HttpURLConnection connUpdate = (HttpURLConnection) updateAccount.openConnection();
                     connUpdate.setRequestMethod("POST");
 
